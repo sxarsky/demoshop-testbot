0eaee2d1f567798f18888b77e6e8b23938c3225e
SahilLamba27 <154925388+SahilLamba27@users.noreply.github.com>
2025-07-09 10:53:29 -0400

    updated server config to support localhost access for trace collection (#9)
    Co-authored-by: Sahil Lamba <slamba@Sahils-MacBook-Pro.local>

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
